c2bd0e8233040e6d3fa92284c37bcdf4416c3d6d
moneymayx <hitchb28@gmail.com>
2025-10-30 00:10:08 -0400

    Fix header logo asset casing and alignment

--- frontend/src/components/TopNavigation.tsx
+++ frontend/src/components/TopNavigation.tsx
@@ -33,7 +33,7 @@ export default function TopNavigation() {
               <img 
                 src="/images/billions-logo.png"
                 alt="BILLIONS$"
-                className="h-24 w-auto"
+                className="h-12 w-auto object-contain"
               />
             </Link>
 
